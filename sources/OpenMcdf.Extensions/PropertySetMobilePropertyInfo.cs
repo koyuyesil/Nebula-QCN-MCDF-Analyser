f524d147931e412c8b6bc22489f6ae4351620df3
KoyuYesil <gen_com@live.com>
2021-07-22 00:27:52 +0300

    extension update

--- sources/OpenMcdf.Extensions/PropertySetMobilePropertyInfo.cs
+++ sources/OpenMcdf.Extensions/PropertySetMobilePropertyInfo.cs
@@ -4,27 +4,44 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.IO;
+using System.ComponentModel;
 
 namespace OpenMcdf.Extensions
 {
+    [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("Summary info for Qualcomm NV Calibration File (QCN)")]
     public class PropertySetMobilePropertyInfo
     {
-
+        [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("Electronic Serial Number")]
         public uint ESN { get; set; }
+
+        [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("Qualcomm Baseband Chip Idendifier")]
         public uint MODEM { get; set; }
+       
+        [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("File Major Version")]
         public byte MajorVersion { get; set; }
+
+        [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("File Minor Version")]
         public byte MinorVersion { get; set; }
+
+        [CategoryAttribute("Pointer"), DescriptionAttribute("Modem Firmware Version string size")]
         public int ModemFirmwareVersionPointer { get; set; }
+
+        [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("Modem Firmware version")]
         public string ModemFirmwareVersion { get; set; }
+        [CategoryAttribute("Pointer"), DescriptionAttribute("Software Version string size")]
         public int SoftwareVersionPointer { get; set; }
+        [CategoryAttribute("Mobile Properties Summary"), DescriptionAttribute("This file created by this software")]
         public string SoftwareVersion { get; set; }
 
         public PropertySetMobilePropertyInfo()
         {
 
         }
-
-        public void Read(System.IO.BinaryReader br)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="br"></param>
+        public void Read(BinaryReader br)
         {
             ESN= br.ReadUInt32();
             MODEM= br.ReadUInt16();
--- sources/Structured Storage Explorer/MainForm.cs
+++ sources/Structured Storage Explorer/MainForm.cs
@@ -14,25 +14,15 @@ using System.Globalization;
 using StructuredStorageExplorer.Properties;
 using Be.Windows.Forms;
 using OpenMcdf.Extensions;
-using OpenMcdf.Extensions;
 using System.Linq;
 using System.Collections;
 
-
-// Author Federico Blaseotto
-
 namespace StructuredStorageExplorer
 {
-
-    /// <summary>
-    /// Sample Structured Storage viewer to 
-    /// demonstrate use of OpenMCDF
-    /// </summary>
     public partial class MainForm : Form
     {
         private CompoundFile cf;
         private FileStream fs;
-
         public MainForm()
         {
             InitializeComponent();
@@ -40,7 +30,6 @@ namespace StructuredStorageExplorer
 #if !OLE_PROPERTY
             tabControl1.TabPages.Remove(tabPage2);
 #endif
-
             //Load images for icons from resx
             Image folderImage = (Image)Properties.Resources.ResourceManager.GetObject("storage");
             Image streamImage = (Image)Properties.Resources.ResourceManager.GetObject("stream");
@@ -51,49 +40,12 @@ namespace StructuredStorageExplorer
             treeView1.ImageList.Images.Add(streamImage);
             //treeView1.ImageList.Images.Add(olePropsImage);
 
-
-
             saveAsToolStripMenuItem.Enabled = false;
             updateCurrentFileToolStripMenuItem.Enabled = false;
 
         }
-
-
-
-        private void OpenFile()
-        {
-            if (!String.IsNullOrEmpty(openFileDialog1.FileName))
-            {
-                CloseCurrentFile();
-
-                treeView1.Nodes.Clear();
-                fileNameLabel.Text = openFileDialog1.FileName;
-                LoadFile(openFileDialog1.FileName, true);
-                canUpdate = true;
-                saveAsToolStripMenuItem.Enabled = true;
-                updateCurrentFileToolStripMenuItem.Enabled = true;
-            }
-        }
-
-        private void CloseCurrentFile()
-        {
-            if (cf != null)
-                cf.Close();
-
-            if (fs != null)
-                fs.Close();
-
-            treeView1.Nodes.Clear();
-            fileNameLabel.Text = String.Empty;
-            saveAsToolStripMenuItem.Enabled = false;
-            updateCurrentFileToolStripMenuItem.Enabled = false;
-
-            propertyGrid1.SelectedObject = null;
-            hexEditor.ByteProvider = null;
-        }
-
+        
         private bool canUpdate = false;
-
         private void CreateNewFile()
         {
             CloseCurrentFile();
@@ -106,7 +58,6 @@ namespace StructuredStorageExplorer
 
             RefreshTree();
         }
-
         private void RefreshTree()
         {
             treeView1.Nodes.Clear();
@@ -118,87 +69,7 @@ namespace StructuredStorageExplorer
 
             //Recursive function to get all storage and streams
             AddNodes(root, cf.RootStorage);
-        }
-
-        private void LoadFile(string fileName, bool enableCommit)
-        {
-
-            fs = new FileStream(
-                fileName,
-                FileMode.Open,
-                enableCommit ?
-                    FileAccess.ReadWrite
-                    : FileAccess.Read
-                );
-
-            try
-            {
-                if (cf != null)
-                {
-                    cf.Close();
-                    cf = null;
-                }
-
-                //Load file
-                if (enableCommit)
-                {
-                    cf = new CompoundFile(fs, CFSUpdateMode.Update, CFSConfiguration.SectorRecycle | CFSConfiguration.NoValidationException | CFSConfiguration.EraseFreeSectors);
-                }
-                else
-                {
-                    cf = new CompoundFile(fs);
-                }
-
-                RefreshTree();
-            }
-            catch (Exception ex)
-            {
-                treeView1.Nodes.Clear();
-                fileNameLabel.Text = String.Empty;
-                MessageBox.Show("Internal error: " + ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        /// <summary>
-        /// Recursive addition of tree nodes foreach child of current item in the storage
-        /// </summary>
-        /// <param name="node">Current TreeNode</param>
-        /// <param name="cfs">Current storage associated with node</param>
-        private void AddNodes(TreeNode node, CFStorage cfs)
-        {
-            Action<CFItem> va = delegate (CFItem target)
-            {
-                TreeNode temp = node.Nodes.Add(
-                    target.Name,
-                    target.Name + (target.IsStream ? " (" + target.Size + " bytes )" : "")
-                    );
-
-                temp.Tag = target;
-
-                if (target.IsStream)
-                {
-
-                    //Stream
-                    temp.ImageIndex = 1;
-                    temp.SelectedImageIndex = 1;
-                }
-                else
-                {
-                    //Storage
-                    temp.ImageIndex = 0;
-                    temp.SelectedImageIndex = 0;
-
-                    //Recursion into the storage
-                    AddNodes(temp, (CFStorage)target);
-                }
-            };
-
-            //Visit NON-recursively (first level only)
-            cfs.VisitEntries(va, false);
-        }
-
-
-
+        } 
         private void exportDataToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //No export if storage
@@ -259,7 +130,6 @@ namespace StructuredStorageExplorer
                 }
             }
         }
-
         private void removeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             TreeNode n = treeView1.SelectedNode;
@@ -267,7 +137,6 @@ namespace StructuredStorageExplorer
 
             RefreshTree();
         }
-
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             saveFileDialog1.FilterIndex = 2;
@@ -276,7 +145,6 @@ namespace StructuredStorageExplorer
                 cf.Save(saveFileDialog1.FileName);
             }
         }
-
         private void updateCurrentFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (canUpdate)
@@ -289,7 +157,6 @@ namespace StructuredStorageExplorer
                 MessageBox.Show("Cannot update a compound document that is not based on a stream or on a file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
         }
-
         private void addStreamToolStripMenuItem_Click(object sender, EventArgs e)
         {
             string streamName = String.Empty;
@@ -314,7 +181,6 @@ namespace StructuredStorageExplorer
                 RefreshTree();
             }
         }
-
         private void addStorageStripMenuItem1_Click(object sender, EventArgs e)
         {
             string storage = String.Empty;
@@ -338,7 +204,6 @@ namespace StructuredStorageExplorer
                 RefreshTree();
             }
         }
-
         private void importDataStripMenuItem1_Click(object sender, EventArgs e)
         {
             string fileName = String.Empty;
@@ -360,24 +225,32 @@ namespace StructuredStorageExplorer
                 }
             }
         }
-
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (cf != null)
                 cf.Close();
         }
+        private void closeStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (this.hexEditor.ByteProvider != null && this.hexEditor.ByteProvider.HasChanges())
+            {
+                if (MessageBox.Show("Do you want to save pending changes ?", "Save changes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    this.hexEditor.ByteProvider.ApplyChanges();
+                }
+            }
 
+            CloseCurrentFile();
+        }
         private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
         {
 
         }
-
         private void newStripMenuItem1_Click(object sender, EventArgs e)
         {
 
             CreateNewFile();
         }
-
         private void openFileMenuItem_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
@@ -392,7 +265,92 @@ namespace StructuredStorageExplorer
                 }
             }
         }
+        private void OpenFile()
+        {
+            if (!String.IsNullOrEmpty(openFileDialog1.FileName))
+            {
+                CloseCurrentFile();
 
+                treeView1.Nodes.Clear();
+                fileNameLabel.Text = openFileDialog1.FileName;
+                LoadFile(openFileDialog1.FileName, true);
+                canUpdate = true;
+                saveAsToolStripMenuItem.Enabled = true;
+                updateCurrentFileToolStripMenuItem.Enabled = true;
+            }
+        }
+        private void LoadFile(string fileName, bool enableCommit)
+        {
+
+            FileStream fileStream = new FileStream(
+                            fileName,
+                            FileMode.Open,
+                            enableCommit ?
+                                FileAccess.ReadWrite
+                                : FileAccess.Read
+                            );
+            fs = fileStream;
+
+            try
+            {
+                if (cf != null)
+                {
+                    cf.Close();
+                    cf = null;
+                }
+
+                //Load file(loadfilesettings add)
+                if (enableCommit)
+                {
+                    //cf = new CompoundFile(fs, CFSUpdateMode.Update, CFSConfiguration.SectorRecycle | CFSConfiguration.NoValidationException | CFSConfiguration.EraseFreeSectors);
+                    cf = new CompoundFile(fs, CFSUpdateMode.Update, CFSConfiguration.SectorRecycle);
+                }
+                else
+                {
+                    cf = new CompoundFile(fs);
+                }
+
+                RefreshTree();
+            }
+            catch (Exception ex)
+            {
+                treeView1.Nodes.Clear();
+                fileNameLabel.Text = String.Empty;
+                MessageBox.Show("Internal error: " + ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void AddNodes(TreeNode node, CFStorage cfs)
+        {
+            Action<CFItem> va = delegate (CFItem target)
+            {
+                TreeNode temp = node.Nodes.Add(
+                    target.Name,
+                    target.Name + (target.IsStream ? " (" + target.Size + " bytes )" : "")
+                    );
+
+                temp.Tag = target;
+
+                if (target.IsStream)
+                {
+
+                    //Stream
+                    temp.ImageIndex = 1;
+                    temp.SelectedImageIndex = 1;
+                }
+                else
+                {
+                    //Storage
+                    temp.ImageIndex = 0;
+                    temp.SelectedImageIndex = 0;
+
+                    //Recursion into the storage
+                    AddNodes(temp, (CFStorage)target);
+                }
+            };
+
+            //Visit NON-recursively (first level only)
+            cfs.VisitEntries(va, false);
+        }
         private void treeView1_MouseUp(object sender, MouseEventArgs e)
         {
             // Get the node under the mouse cursor.
@@ -434,31 +392,13 @@ namespace StructuredStorageExplorer
                         BinaryReader br = new BinaryReader(sy);
                         mi.Read(br);
 
-                          DataTable ds = new DataTable();
+                        propertyGrid1.SelectedObject = mi;
+
+                        DataTable ds = new DataTable();
 
                         ds.Columns.Add("Name", typeof(String));
                         ds.Columns.Add("Type", typeof(String));
                         ds.Columns.Add("Value", typeof(String));
-                        //foreach (var p in c.Properties)
-                        //{
-                        //    if (p.Value.GetType() != typeof(byte[]) && p.Value.GetType().GetInterfaces().Any(t => t == typeof(IList)))
-                        //    {
-                        //        for (int h = 0; h < ((IList)p.Value).Count; h++)
-                        //        {
-                        //            DataRow dr = ds.NewRow();
-                        //            dr.ItemArray = new Object[] { p.PropertyName, p.VTType, ((IList)p.Value)[h] };
-                        //            ds.Rows.Add(dr);
-                        //        }
-                        //    }
-                        //    else
-                        //    {
-                        //        DataRow dr = ds.NewRow();
-                        //        dr.ItemArray = new Object[] { p.PropertyName, p.VTType, p.Value };
-                        //        ds.Rows.Add(dr);
-                        //    }
-                        //}
-                        //ds.AcceptChanges();
-                        //dgvOLEProps.DataSource = ds;
                     }
 #endif
                 }
@@ -474,7 +414,7 @@ namespace StructuredStorageExplorer
 
             if (n != null)
             {
-                propertyGrid1.SelectedObject = n.Tag;
+                //propertyGrid1.SelectedObject = n.Tag;
                 CFStream targetStream = n.Tag as CFStream;
                 if (targetStream != null)
                 {
@@ -486,19 +426,22 @@ namespace StructuredStorageExplorer
                 }
             }
         }
-
-        private void closeStripMenuItem1_Click(object sender, EventArgs e)
+        private void CloseCurrentFile()
         {
-            if (this.hexEditor.ByteProvider != null && this.hexEditor.ByteProvider.HasChanges())
-            {
-                if (MessageBox.Show("Do you want to save pending changes ?", "Save changes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
-                {
-                    this.hexEditor.ByteProvider.ApplyChanges();
-                }
-            }
+            if (cf != null)
+                cf.Close();
 
-            CloseCurrentFile();
+            if (fs != null)
+                fs.Close();
+
+            treeView1.Nodes.Clear();
+            fileNameLabel.Text = String.Empty;
+            saveAsToolStripMenuItem.Enabled = false;
+            updateCurrentFileToolStripMenuItem.Enabled = false;
+
+            propertyGrid1.SelectedObject = null;
+            hexEditor.ByteProvider = null;
         }
-
+        
     }
 }
